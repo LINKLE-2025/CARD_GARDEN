b4cd161b25bed1c847ba0d4f8980e3736751b2ea
Gaegulin <121297188+Gaegulin@users.noreply.github.com>
2025-06-26 09:23:13 +0900

    feat: likecard 트리거 추가

--- src/main/resources/sql/ddl/schema.sql
+++ src/main/resources/sql/ddl/schema.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:24dfca42ec8025175bc05bb18b49f6216386deb1f4719743ba2136c25e635a58
-size 8990
+oid sha256:b0366af8c323047b06faabefc180157f48c0a4dd5e501cc1765acd4455341842
+size 9519
