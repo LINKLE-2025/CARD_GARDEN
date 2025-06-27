c9fdc6d8aaafd4f5aef023d87613d7a5d07e731a
ljhpaul <colosair@gmail.com>
2025-06-27 17:08:23 +0900

    feat: 회원가입 제약조건 추가
    feat: 회원가입 제약조건 추가

--- src/main/resources/sql/ddl/schema_sample.sql
+++ src/main/resources/sql/ddl/schema_sample.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ada0ea3cabf5ea1a783fc01ff7f449d16c926ed830aaf909eb3783f4e8db8259
-size 627
+oid sha256:38f89febdf69946773ecef67cc1589416121fbb9c2d272e6aac01f5e5c663fe8
+size 716
